d8ad65f63d509abfaf34d2eda04b827e1d42c6c8
matiahi <matiahi@yahoo.com>
2025-04-30 13:19:48 -0600

    Added input fields for custom timer duration and label

--- src/SimpleTimer.java
+++ src/SimpleTimer.java
@@ -1,9 +1,12 @@
 
+import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
 
 public class SimpleTimer {
 
-    private static int secondsRemaining = (1 * 60);
+    private static int timeRemaining;
+    private static Timer timer;
 
     public static void main(String[] args) throws Exception {
 
@@ -11,41 +14,56 @@ public class SimpleTimer {
         JFrame frame = new JFrame("Simple Timer");
         frame.setSize(300, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new GridLayout(4, 1));
+
+        JTextField timeInput = new JTextField("0");
+        JTextField lableInput = new JTextField("Timer");
 
         // Build the Label for Time
-        JLabel label = new JLabel(formatTime(secondsRemaining), SwingConstants.CENTER);
+        JLabel label = new JLabel("00:00", SwingConstants.CENTER);
+        JLabel customLabel = new JLabel("", SwingConstants.CENTER);
 
         JButton startButton = new JButton("Start");
-        startButton.addActionListener(e -> startTimer(label));
 
-        // Setting for the Window
-        JPanel panel = new JPanel();
-        panel.add(label);
-        panel.add(startButton);
-        frame.add(panel);
+        frame.add(new JLabel("Time:", SwingConstants.CENTER));
+        frame.add(timeInput);
 
+        frame.add(new JLabel("Label:", SwingConstants.CENTER));
+        frame.add(lableInput);
+        frame.add(startButton);
+        frame.add(customLabel);
+        frame.add(label);
+
+        startButton.addActionListener(e -> {
+            try {
+                timeRemaining = Integer.parseInt(timeInput.getText()) * 60;
+                String userLabel = lableInput.getText();
+                customLabel.setText("Label:" + userLabel);
+
+                if (timer != null && timer.isRunning()) {
+                    timer.stop();
+                }
+
+                timer = new Timer(1000, new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        int minutes = timeRemaining / 60;
+                        int seconds = timeRemaining % 60;
+                        label.setText(String.format("%02d:%02d", minutes, seconds));
+                        timeRemaining--;
+
+                        if (timeRemaining < 0) {
+                            timer.stop();
+                            label.setText("End!");
+                        }
+                    }
+                });
+                timer.start();
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(frame, "Please just enter positive integer.");
+            }
+        });
         frame.setVisible(true);
 
     }
 
-    public static void startTimer(JLabel label) {
-        Timer timer = new Timer(1000, e -> {
-            secondsRemaining--;
-
-            label.setText(formatTime(secondsRemaining));
-
-            if (secondsRemaining <= 0) {
-                ((Timer) e.getSource()).stop();
-                JOptionPane.showMessageDialog(null, "Times end");
-
-            }
-        });
-        timer.start();
-    }
-
-    public static String formatTime(int seconds) {
-        int minutes = seconds / 60;
-        int remainingSecond = seconds % 60;
-        return String.format("%02d:%02d", minutes, remainingSecond);
-    }
 }
